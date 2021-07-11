29b1c02d81af1b93c0a87bea62e2c51302b76cc3
Ale Henestroza <alehenestroza@gmail.com>
2021-07-11 20:52:07 -0300

    Agregada documentación del proyecto y script para regenerar la base de datos

--- TP 4/Test/Program.cs
+++ TP 4/Test/Program.cs
@@ -9,7 +9,7 @@ using Entidades.Productos.Bicicletas;
 using Entidades.Productos.Componentes;
 using Entidades.Productos.Materiales;
 using Entidades.Productos.Accesorios;
-using Entidades.DAO.Orden;
+using Entidades.Datos;
 
 namespace Test
 {
@@ -17,31 +17,26 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            // Revisa si se recuperan correctamente los datos de la BBDD
-            //Console.WriteLine(Costos.CostoAcero);
-            //Console.WriteLine(Stock.CantAcero);
-            //StockDAO stockDAO = new StockDAO();
-            //Dictionary<string, string> data = stockDAO.SelectStock();
-            //foreach(KeyValuePair<string, string> asd in data)
-            //{
-            //    Console.WriteLine(asd.Key);
-            //    Console.WriteLine(asd.Value);
-            //}
+            //Revisa si se recuperan correctamente los datos de la BBDD
+            Console.WriteLine(Costos.CostoAcero);
+            Console.WriteLine(Stock.CantAcero);
+            StockDAO stockDAO = new StockDAO();
+            Dictionary<string, string> data = stockDAO.SelectStock();
+            foreach (KeyValuePair<string, string> asd in data)
+            {
+                Console.WriteLine(asd.Key);
+                Console.WriteLine(asd.Value);
+            }
 
-            //Casco casco = new Casco(57);
-            //Luz luz = new Luz("Amarillo");
-            //Bicicleta<Acero> mb = new MountainBike<Acero>(new Rueda(26), new Rueda(26), 58, false);
-            //Bicicleta<Aluminio> p = new Playera<Aluminio>(new Rueda(26), new Rueda(26), 58, true);
+            // Se revisa si se muestra correctamente el texto de una orden
+            Casco casco = new Casco(57);
+            Luz luz = new Luz("Amarillo");
+            Bicicleta<Acero> mb = new MountainBike<Acero>(new Rueda(26), new Rueda(26), 58, false);
+            Bicicleta<Aluminio> p = new Playera<Aluminio>(new Rueda(26), new Rueda(26), 58, true);
 
-            //Orden orden = new Orden(mb, 2, p, 1, casco, 3, luz, 5);
-            //Console.WriteLine(orden.ToString());
+            Orden orden = new Orden(1, mb, 2, p, 1, casco, 3, luz, 5);
 
-            //Dictionary<string, int> data = new Dictionary<string, int>();
-            //data.Add("Hola", 123);
-            //Console.WriteLine(data.Keys.First());
-
-            Console.WriteLine(bool.Parse("1"));
-            Console.WriteLine(bool.Parse("0"));
+            Console.WriteLine(orden.MostrarOrden());
 
             Console.ReadKey();
         }
